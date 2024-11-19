1c31b9218ddc3c82d23fde869e202c75c18e083e
Rahul Kumar <rahulkumar@Rahuls-MacBook-Air.local>
2024-11-19 15:13:39 +0530

    modified: script to get .env on run dev added: prettierc

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("hello world!");
+import express from 'express';
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get('/hello-world', async (req, res) => {
+    res.send('req received');
+});
+
+const PORT = process.env.PORT;
+if (!PORT) throw Error('Port for server is not provided');
+
+app.listen(PORT, () => console.log(`server listening on ${PORT}`));
